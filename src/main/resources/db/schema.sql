77b6c7852761ac363771abc4a56e43139694fb43
Aleksander Obrestad <aleksander@bekk-mac-3480.local>
2025-09-03 14:56:14 +0200

    updated db schema with additional tables

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -2,3 +2,16 @@ CREATE TABLE IF NOT EXISTS securityChampions (
     id  VARCHAR(60) PRIMARY KEY,
     email   VARCHAR(60)
  );
+
+CREATE TABLE IF NOT EXISTS repositories (
+    id  VARCHAR(60) PRIMARY KEY,
+    repoName   VARCHAR(60)
+    );
+
+CREATE TABLE IF NOT EXISTS securityChampion_repository (
+    repoID VARCHAR(60),
+    secChampID VARCHAR(60),
+    CONSTRAINT repo FOREIGN KEY (repoID)  REFERENCES repositories(id),
+    CONSTRAINT securityChampion FOREIGN KEY (secChampID) REFERENCES securityChampions(id)
+
+    );
