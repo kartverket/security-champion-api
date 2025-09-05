6377c08b29fce1ace9fdfa9143f52efe38fa0f79
Aleksander Obrestad <aleksander@bekk-mac-3480.local>
2025-09-05 15:28:50 +0200

    wip flyway2

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
 }
 
 kotlin {
