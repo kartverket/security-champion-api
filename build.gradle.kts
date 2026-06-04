126e86571dcf55c4dfaa6bc8819c4783816d92cb
Håkon Frøland <hakon.froland@gmail.com>
2026-06-04 10:03:56 +0200

    Legger til distribuert tracing
    - Distribuert tracing er aktivert med OpenTelemetry via OTLP eksport.
    - Standard endpoint er `http://localhost:4318/v1/traces`
    - Tracesampling er satt til 1.0 -> alt blir samplet

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,8 @@ ext["postgresql.version"] = "42.7.11"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("io.micrometer:micrometer-tracing-bridge-otel")
+    implementation("io.opentelemetry:opentelemetry-exporter-otlp")
     implementation("net.logstash.logback:logstash-logback-encoder:8.1")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
