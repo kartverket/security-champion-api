7c12c7cc66481e136c3a76614a8f79e1f864cf8b
Håkon Frøland <hakon.froland@gmail.com>
2026-06-04 12:36:42 +0200

    Fjerner wildcard imports

--- src/main/kotlin/com/example/securitychampionapi/controller/SecurityChampionController.kt
+++ src/main/kotlin/com/example/securitychampionapi/controller/SecurityChampionController.kt
@@ -1,6 +1,12 @@
 package com.example.securitychampionapi.controller
 
-import com.example.securitychampionapi.controller.models.*
+import com.example.securitychampionapi.controller.models.GetSecurityChampionsBody
+import com.example.securitychampionapi.controller.models.GetSecurityChampionsResponse
+import com.example.securitychampionapi.controller.models.SecurityChampionResponse
+import com.example.securitychampionapi.controller.models.SetSecurityChampionBody
+import com.example.securitychampionapi.controller.models.SetSecurityChampionResponse
+import com.example.securitychampionapi.controller.models.SetSecurityChampionWithNoRepoBody
+import com.example.securitychampionapi.controller.models.SetSecurityChampionsBody
 import com.example.securitychampionapi.service.SecurityChampionService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
